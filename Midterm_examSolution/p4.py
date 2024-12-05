3c69be444625947bc10a0456e044426ada3ccbfc
sadiaNz <123831842+sadiaNz@users.noreply.github.com>
2024-12-05 16:28:39 -0500

    Update p4.py

--- Midterm_examSolution/p4.py
+++ Midterm_examSolution/p4.py
@@ -16,11 +16,13 @@ except:
 df = pd.read_csv(url)
 
 filtered_cities = df[df['population'] > 1000000]
+print('Result of cities that have more than 1 million population')
 print(filtered_cities)
 # Step 2:  using the “lat” (for latitude) and “lng” (for longitude) columns for haversine_distances API and compute pairwise haversine distances among these big cities.
 lat_lng = np.radians(filtered_cities[['lat', 'lng']].values)
 earth_radius = 6371000/1000
 distances = haversine_distances(lat_lng) * earth_radius  # Convert radians to kilometres
+print('\n\\n pairwise haversine distances among these big cities')
 print(distances)
 # Step 3: apply either DBSCAN or AgglomerativeClustering, to cluster the big cities into clusters using the parameters of your choice. Note that the two important parameters for AgglomerativeClustering are n_clusters (e.g. 10) and linkage( e.g., "average"), and  those for DBSCAN are eps (e.g., 10km) and min_samples (e.g., 5). Plot your results for visual examination using any visualization package (e.g., Matplotlib).
 # using DBSCAN
