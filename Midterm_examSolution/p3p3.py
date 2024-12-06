47c04520922f3dd2ce2c68650f3a3c20f34346f4
sadiaNz <123831842+sadiaNz@users.noreply.github.com>
2024-12-06 14:11:10 -0500

    Update p3p3.py

--- Midterm_examSolution/p3p3.py
+++ Midterm_examSolution/p3p3.py
@@ -4,7 +4,8 @@ import time
 
 # File paths
 parquet_file = "/data/csc59866_f24/tlcdata/yellow_tripdata_2009-01.parquet"
-csv_file = "/home/nawa24/Midterm_Cude/Midterm_examSolution/csv_files/yellow_tripdata_2009-01.csv"
+csv_file = "csv_files/yellow_tripdata_2009-01.csv"
+
 
 # ---------------- Read Parquet and Save as CSV ----------------
 print("Step 1: Reading Parquet file...")
